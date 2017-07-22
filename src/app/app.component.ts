91e360af2c54523b8e9ca0491c8c8654e9f7ef98
PC\Shergat <cbellom@unal.edu.co>
2017-07-21 20:27:27 -0500

    :v

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Data }    from './model/data';
+import * as pdfMake from 'pdfmake/build/pdfmake.js';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts.js';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-}
+
+    powers = ['Really Smart', 'Super Flexible',
+              'Super Hot', 'Weather Changer'];
+
+    model = new Data(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+
+    submitted = false;
+
+    onSubmit() {
+      this.submitted = true;
+      this.createPdf();
+    }
+
+    newHero() {
+      this.model = new Data(42, '', '');
+    }
+
+    createPdf(){
+      let docDefinition = { content: 'This is an sample PDF printed with pdfMake' };
+      // open the PDF in a new window
+      pdfMake.createPdf(docDefinition).download('optionalName.pdf');
+    }
+  }
---
+++ src/app/model/data.ts
@@ -0,0 +1,10 @@
+export class Data {
+
+  constructor(
+    public id: number,
+    public name: string,
+    public power: string,
+    public alterEgo?: string
+  ) {  }
+
+}
